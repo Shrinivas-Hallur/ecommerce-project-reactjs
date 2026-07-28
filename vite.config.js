67c72fedf3cd558726b8acabd8c79f2f21f03b2b
Shrinivas-Hallur <shrinivasphallur@gmail.com>
2026-07-28 23:07:00 +0530

    main changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ecommerce-project-reactjs/",
   plugins: [react({
   babel: {
     plugins: [['babel-plugin-react-compiler']],
