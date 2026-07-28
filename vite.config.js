f11decb6df3eb1abb6ea886f8451a057ce745ce2
Shrinivas-Hallur <shrinivasphallur@gmail.com>
2026-07-28 22:53:37 +0530

    cinfig changes

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,30 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react({
-//   babel: {
-//     plugins: [['babel-plugin-react-compiler']],
-//   },
-// })],
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target: 'http://localhost:3000'
-//       },
-//       '/images':{
-//         target: 'http://localhost:3000'
-//       }
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/ecommerce-project-reactjs/',
-  plugins: [react()],
+  plugins: [react({
+  babel: {
+    plugins: [['babel-plugin-react-compiler']],
+  },
+})],
+  server:{
+    proxy:{
+      '/api':{
+        target: 'http://localhost:3000'
+      },
+      '/images':{
+        target: 'http://localhost:3000'
+      }
+    }
+  }
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/ecommerce-project-reactjs/',
+//   plugins: [react()],
+// })
 
